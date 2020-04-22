28e77f8600216dd8e7af364a7c7c9452acabdef4
Pavel Kalnitski <47810563+kalnitski@users.noreply.github.com>
2020-04-22 20:26:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,49 @@ const parseLineString = (string) => {
     let arr = item.split(" ");
 
     geoTags.push({
-      lat: parseFloat(arr[0]),
-      lon: parseFloat(arr[1])
+      lat: parseFloat(arr[1]),
+      lon: parseFloat(arr[0])
     });
   });
 
   return geoTags;
 }
-const routeToArray = (data) => {
+const parseStop = (string) => {
+
+  string = string.replace(/\)/gm, "").replace(/\(/gm, "").replace(/POINT/gm, "");
+
+  let arr = string.split(" ");
+
+  return {
+    lat: parseFloat(arr[1]),
+    lon: parseFloat(arr[0])
+  }
+}
+const routeToStops = (data) => {
+  const func = (arr) => {
+    let geoTags = [];
+
+    arr.forEach((item, i) => {
+      geoTags.push({
+        ...parseStop(item.geometry.centroid),
+        name: item.name,
+        time: 0,
+        busStop: true
+      })
+    });
+
+    return geoTags;
+  }
+
+  let forward = data.result.items[0].directions[0].platforms;
+  let backward = data.result.items[0].directions[1].platforms;
+
+  return {
+    forward: func(forward),
+    backward: func(backward)
+  }
+}
+const routeToShape = (data) => {
   let forward = data.result.items[0].directions[0].geometry.selection;
   let backward = data.result.items[0].directions[1].geometry.selection;
 
@@ -22,5 +57,11 @@ const routeToArray = (data) => {
     backward: parseLineString(backward)
   }
 }
+const routeToObject = (data) => {
+  return {
+    busStops: routeToStops(data),
+    shape: routeToShape(data)
+  }
+}
 
-module.exports = routeToArray;
+exports.routeToObject = routeToObject;
